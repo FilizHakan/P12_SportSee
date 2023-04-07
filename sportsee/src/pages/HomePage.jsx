082c4937c8c779e81ec18a7cf3bc867618132394
Filiz <filizhakan13@gmail.com>
2023-04-07 17:47:46 +0200

    Installation dependencies / service / back

---
+++ sportsee/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+    return (
+        <div>
+            Toto
+        </div>
+    )
+}
+
+export default HomePage;
---
+++ sportsee/src/service/useFetch.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url)=>
+{
+    // Use hooks with useState (re-display) and useEffect
+    const [data, setData] =useState(null);
+    const [error, setError] = useState(null);
+    const [isDataLoading, setDataLoading] = useState(true);
+
+    // useEffect is executed when page is displayed
+    useEffect(()=>
+    {
+            const fetchData = async () => {
+                const result = await fetch(url);
+                const json = await result.json();
+
+                setData(json);
+                setDataLoading(false);
+            }
+            
+            fetchData()
+                .catch((err)=>
+                {
+                    setError(err.message);
+                    setDataLoading(false);
+                    throw Error("Pas de panique, c'est juste une erreur");    
+                });       
+    }, [url] );
+
+    return { data, error, isDataLoading };
+};
+    
+export default useFetch;
